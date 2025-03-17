f8c1de9e3df67bd0e3b088202e15e5d5995c66a0
Scotte Zinn <scotte@zinn.ca>
2025-03-17 08:12:53 -0400

    chore: Cleanup old implementation

--- old/apps/rust-musl-chef/ci/latest.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-channel=$1
-version=$(curl -sX GET https://api.github.com/repos/rust-lang/rust/releases/latest | jq --raw-output '. | .tag_name'  | head -n1)
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
